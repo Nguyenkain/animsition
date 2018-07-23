89b1605b293623a825e528a8beb1b28ff75aea3e
Nguyenkain <nguyenkain@gmail.com>
2018-07-23 13:54:57 +0700

    Update animsition.js

--- dist/js/animsition.js
+++ dist/js/animsition.js
@@ -16,6 +16,10 @@
   }
 }(function ($) {
   'use strict';
+  var windowLoaded = false;
+  $(window).on("load", function() {
+    windowLoaded = true;
+  });
   var namespace = 'animsition';
   var __ = {
     init: function(options){
@@ -101,10 +105,15 @@
           if(options.timeout) __.addTimer.call(_this);
 
           if(options.onLoadEvent) {
-            $window.on('load.' + namespace, function() {
+            if(!windowLoaded) {
+              $window.on('load.' + namespace, function() {
+                if(__.settings.timer) clearTimeout(__.settings.timer);
+                __.in.call(_this);
+              });
+            } else {
               if(__.settings.timer) clearTimeout(__.settings.timer);
-              __.in.call(_this);
-            });
+                __.in.call(_this);
+            }
           }
 
           $window.on('pageshow.' + namespace, function(event) {
